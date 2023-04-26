a6f4c250a9c167cde816ff8b4991d2c387d88b27
Stevö <stevehuwjohn@gmail.com>
2023-04-26 13:42:07 +0100

    WIP

--- src/Zen.Z80/Processor/Core.cs
+++ src/Zen.Z80/Processor/Core.cs
@@ -2,4 +2,23 @@
 
 public class Core
 {
+    private readonly Interface _interface;
+
+    private readonly State _state;
+
+    private readonly Dictionary<int, Instruction> _instructions = new();
+
+    public Core()
+    {
+        _interface = new Interface();
+
+        _state = new State();
+    }
+
+    public Core(Interface @interface, State state)
+    {
+        _interface = @interface;
+
+        _state = state;
+    }
 }
---
+++ src/Zen.Z80/Processor/Instruction.cs
@@ -0,0 +1,27 @@
+﻿namespace Zen.Z80.Processor;
+
+public class Instruction
+{
+    public Func<Interface, State, int> Execute { get; private set; }
+
+    public string Mnemonic { get; private set; }
+
+    public uint OpCode { get; private set; }
+
+    public int ParameterLength { get; private set; }
+
+    public string TraceTemplate { get; private set; }
+
+    public Instruction(Func<Interface, State, int> execute, string mnemonic, uint opCode, int parameterLength, string traceTemplate)
+    {
+        Execute = execute;
+
+        Mnemonic = mnemonic;
+
+        OpCode = opCode;
+
+        ParameterLength = parameterLength;
+
+        TraceTemplate = traceTemplate;
+    }
+}
