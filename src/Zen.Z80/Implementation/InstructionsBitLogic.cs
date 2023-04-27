277807bd5c0515fbeeb111c6b6074d7cdb949735
Stevö <stevehuwjohn@gmail.com>
2023-04-27 10:40:55 +0100

    WIP

--- src/Zen.Z80/Implementation/InstructionsBitLogic.cs
+++ src/Zen.Z80/Implementation/InstructionsBitLogic.cs
@@ -97,6 +97,42 @@ public partial class Instructions
         _state.SetMCycles(4, 4, 3, 5, 4, 3);
     }
 
+    public void RL_IX_d_R(RegisterPair source, byte[] parameters, Register? target = null)
+    {
+        unchecked
+        {
+            var address = _state[source];
+
+            address = (ushort) (address + (sbyte) parameters[0]);
+
+            var data = _interface.ReadFromMemory(address);
+
+            var topBit = (byte) ((data & 0x80) >> 7);
+
+            var result = (byte) (((data << 1) & 0xFE) | (byte) (_state[Flag.Carry] ? 0x01 : 0x00));
+
+            _interface.WriteToMemory(address, result);
+
+            if (target != null)
+            {
+                _state[(Register) target] = result;
+            }
+
+            _state[Flag.Carry] = topBit == 1;
+            _state[Flag.AddSubtract] = false;
+            _state[Flag.ParityOverflow] = result.IsEvenParity();
+            _state[Flag.X1] = (address & 0x08) > 0;
+            _state[Flag.HalfCarry] = false;
+            _state[Flag.X2] = (address & 0x20) > 0;
+            _state[Flag.Zero] = result == 0;
+            _state[Flag.Sign] = (sbyte) result < 0;
+
+            _state.MemPtr = address;
+        }
+
+        _state.SetMCycles(4, 4, 3, 5, 4, 3);
+    }
+
     public void RRC_IX_d_R(RegisterPair source, byte[] parameters, Register? target = null)
     {
         unchecked
--- src/Zen.Z80/Implementation/Instructions_InitialiseDDCB.cs
+++ src/Zen.Z80/Implementation/Instructions_InitialiseDDCB.cs
@@ -13,7 +13,6 @@ public partial class Instructions
         {
 #pragma warning disable CS8509
             var register = registerNumber switch
-#pragma warning restore CS8509
             {
                 0 => Register.B,
                 1 => Register.C,
@@ -24,10 +23,13 @@ public partial class Instructions
                 6 => (Register?) null,
                 7 => Register.A
             };
+#pragma warning restore CS8509
 
             _instructions.Add(0xDDCB00, new Instruction(d => RLC_IX_d_R(RegisterPair.IX, d, register), $"RLC (IX + d){(register == null ? string.Empty : $", {register}")}", 0xDDCB00, 0));
 
             _instructions.Add(0xDDCB08, new Instruction(d => RRC_IX_d_R(RegisterPair.IX, d, register), $"RRC (IX + d){(register == null ? string.Empty : $", {register}")}", 0xDDCB08, 0));
+
+            _instructions.Add(0xDDCB10, new Instruction(d => RL_IX_d_R(RegisterPair.IX, d, register), $"RL (IX + d){(register == null ? string.Empty : $", {register}")}", 0xDDCB10, 0));
         }
 
         for (var bit = 0; bit < 8; bit++)
@@ -38,7 +40,6 @@ public partial class Instructions
             {
 #pragma warning disable CS8509
                 var register = registerNumber switch
-#pragma warning restore CS8509
                 {
                     0 => Register.B,
                     1 => Register.C,
@@ -49,6 +50,7 @@ public partial class Instructions
                     6 => (Register?) null,
                     7 => Register.A
                 };
+#pragma warning restore CS8509
 
                 _instructions.Add(0xDDCB40 + registerNumber + bit * 8, new Instruction(d => BIT_b_IX_d((byte) (1 << innerBit), RegisterPair.IX, d), $"BIT {bit}, (IX + d)", 0xDDCB40 + registerNumber + bit * 8, 0));
 
