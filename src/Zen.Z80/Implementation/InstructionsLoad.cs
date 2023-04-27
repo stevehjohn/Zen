bb8b2a0e06018ba31d2392ce9d07d96dce0f31b8
Stevö <stevehuwjohn@gmail.com>
2023-04-27 13:13:45 +0100

    WIP

--- src/Zen.Z80/Implementation/InstructionsLoad.cs
+++ src/Zen.Z80/Implementation/InstructionsLoad.cs
@@ -5,35 +5,53 @@ namespace Zen.Z80.Implementation;
 
 public partial class Instructions
 {
-    private void LD_RR_nn(RegisterPair target, byte[] parameters)
+    private void LD_R_R(Register target, Register source)
     {
-        _state.LoadRegisterPair(target, parameters);
+        unchecked
+        {
+            var value = _state[source];
 
-        _state.Q = 0;
+            _state[target] = value;
 
-        _state.SetMCycles(4, 3, 3);
+            if (target == Register.A && (source == Register.I || source == Register.R))
+            {
+                // Flags
+                // Carry unaffected
+                _state[Flag.AddSubtract] = false;
+                _state[Flag.ParityOverflow] = _state.InterruptFlipFlop2;
+                _state[Flag.X1] = (value & 0x08) > 0;
+                _state[Flag.HalfCarry] = false;
+                _state[Flag.X2] = (value & 0x20) > 0;
+                _state[Flag.Zero] = value == 0;
+                _state[Flag.Sign] = (sbyte) value < 0;
+            }
+            else
+            {
+                _state.Q = 0;
+            }
+        }
+
+        _state.SetMCycles(4);
     }
 
-    private void LD_R_R(Register source, Register target)
+    private void LD_R_aRR(Register target, RegisterPair source)
     {
-        var value = _state[source];
+        unchecked
+        {
+            var address = _state[source];
 
-        _state[target] = value;
-        
-        if (target == Register.A && (source == Register.I || source == Register.R))
-        {
-            // Flags
-            // Carry unaffected
-            _state[Flag.AddSubtract] = false;
-            _state[Flag.ParityOverflow] = _state.InterruptFlipFlop2;
-            _state[Flag.X1] = (value & 0x08) > 0;
-            _state[Flag.HalfCarry] = false;
-            _state[Flag.X2] = (value & 0x20) > 0;
-            _state[Flag.Zero] = value == 0;
-            _state[Flag.Sign] = (sbyte) value < 0;
+            _state[target] = _interface.ReadFromMemory(address);
         }
-        else
+
+        _state.SetMCycles(4, 3);
+    }
+
+    private void LD_RR_nn(RegisterPair target, byte[] parameters)
+    {
+        unchecked
         {
+            _state.LoadRegisterPair(target, parameters);
+
             _state.Q = 0;
         }
 
--- src/Zen.Z80/Implementation/Instructions_Initialise_Base.cs
+++ src/Zen.Z80/Implementation/Instructions_Initialise_Base.cs
@@ -16,6 +16,39 @@ public partial class Instructions
 
         _instructions.Add(0x21, new Instruction(d => LD_RR_nn(RegisterPair.HL, d), "LD HL, nn", 0x21, 2));
 
+        for (var target = 0; target < 8; target++)
+        {
+#pragma warning disable CS8509
+            var targetRegister = target switch
+            {
+                0 => Register.B,
+                1 => Register.C,
+                2 => Register.D,
+                3 => Register.E,
+                4 => Register.H,
+                5 => Register.L,
+                6 => (Register?) null,
+                7 => Register.A
+            };
+#pragma warning restore CS8509
+
+            _instructions.Add(0x40 + target * 8, new Instruction(_ => LD_R_R((Register) targetRegister!, Register.B), $"LD {targetRegister}, B", 0x40 + target * 8, 0));
+
+            _instructions.Add(0x41 + target * 8, new Instruction(_ => LD_R_R((Register) targetRegister!, Register.C), $"LD {targetRegister}, C", 0x41 + target * 8, 0));
+
+            _instructions.Add(0x42 + target * 8, new Instruction(_ => LD_R_R((Register) targetRegister!, Register.D), $"LD {targetRegister}, D", 0x42 + target * 8, 0));
+
+            _instructions.Add(0x43 + target * 8, new Instruction(_ => LD_R_R((Register) targetRegister!, Register.E), $"LD {targetRegister}, E", 0x43 + target * 8, 0));
+
+            _instructions.Add(0x44 + target * 8, new Instruction(_ => LD_R_R((Register) targetRegister!, Register.H), $"LD {targetRegister}, H", 0x44 + target * 8, 0));
+
+            _instructions.Add(0x45 + target * 8, new Instruction(_ => LD_R_R((Register) targetRegister!, Register.L), $"LD {targetRegister}, L", 0x45 + target * 8, 0));
+
+            _instructions.Add(0x46 + target * 8, new Instruction(_ => LD_R_aRR((Register) targetRegister!, RegisterPair.HL), $"LD {targetRegister}, (HL)", 0x46 + target * 8, 0));
+
+            _instructions.Add(0x47 + target * 8, new Instruction(_ => LD_R_R((Register) targetRegister!, Register.A), $"LD {targetRegister}, A", 0x47 + target * 8, 0));
+        }
+
         _instructions.Add(0xCB, new Instruction(_ => PREFIX(0xCB), "PREFIX 0xCB", 0xCB, 0));
 
         _instructions.Add(0xDD, new Instruction(_ => PREFIX(0xDD), "PREFIX 0xDD", 0xDD, 0));
