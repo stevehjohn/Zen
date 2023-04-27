71683c0b465be1eafa112841b2a0043f8d79972e
Stevö <stevehuwjohn@gmail.com>
2023-04-27 10:56:40 +0100

    WIP

--- src/Zen.Z80/Implementation/InstructionsBitLogic.cs
+++ src/Zen.Z80/Implementation/InstructionsBitLogic.cs
@@ -60,6 +60,42 @@ public partial class Instructions
 
         _state.SetMCycles(4, 4, 3, 5, 4, 3);
     }
+    
+    public void RL_aRRd_R(RegisterPair source, byte[] parameters, Register? target = null)
+    {
+        unchecked
+        {
+            var address = _state[source];
+
+            address = (ushort) (address + (sbyte) parameters[0]);
+
+            var data = _interface.ReadFromMemory(address);
+
+            var topBit = (byte) ((data & 0x80) >> 7);
+
+            var result = (byte) (((data << 1) & 0xFE) | (byte) (_state[Flag.Carry] ? 0x01 : 0x00));
+
+            _interface.WriteToMemory(address, result);
+
+            if (target != null)
+            {
+                _state[(Register) target] = result;
+            }
+
+            _state[Flag.Carry] = topBit == 1;
+            _state[Flag.AddSubtract] = false;
+            _state[Flag.ParityOverflow] = result.IsEvenParity();
+            _state[Flag.X1] = (address & 0x08) > 0;
+            _state[Flag.HalfCarry] = false;
+            _state[Flag.X2] = (address & 0x20) > 0;
+            _state[Flag.Zero] = result == 0;
+            _state[Flag.Sign] = (sbyte) result < 0;
+
+            _state.MemPtr = address;
+        }
+
+        _state.SetMCycles(4, 4, 3, 5, 4, 3);
+    }
 
     public void RLC_aRRd_R(RegisterPair source, byte[] parameters, Register? target = null)
     {
@@ -96,8 +132,8 @@ public partial class Instructions
 
         _state.SetMCycles(4, 4, 3, 5, 4, 3);
     }
-
-    public void RL_aRRd_R(RegisterPair source, byte[] parameters, Register? target = null)
+    
+    public void RR_aRRd_R(RegisterPair source, byte[] parameters, Register? target = null)
     {
         unchecked
         {
@@ -107,9 +143,9 @@ public partial class Instructions
 
             var data = _interface.ReadFromMemory(address);
 
-            var topBit = (byte) ((data & 0x80) >> 7);
+            var bottomBit = (byte) (data & 0x01);
 
-            var result = (byte) (((data << 1) & 0xFE) | (byte) (_state[Flag.Carry] ? 0x01 : 0x00));
+            var result = (byte) ((data >> 1) | (byte) (_state[Flag.Carry] ? 0x80 : 0x00));
 
             _interface.WriteToMemory(address, result);
 
@@ -118,7 +154,7 @@ public partial class Instructions
                 _state[(Register) target] = result;
             }
 
-            _state[Flag.Carry] = topBit == 1;
+            _state[Flag.Carry] = bottomBit  == 1;
             _state[Flag.AddSubtract] = false;
             _state[Flag.ParityOverflow] = result.IsEvenParity();
             _state[Flag.X1] = (address & 0x08) > 0;
--- src/Zen.Z80/Implementation/Instructions_InitialiseDDCB.cs
+++ src/Zen.Z80/Implementation/Instructions_InitialiseDDCB.cs
@@ -9,6 +9,8 @@ public partial class Instructions
 {
     private void InitialiseDDCBInstructions()
     {
+        var registerPair = RegisterPair.IX;
+
         for (var registerNumber = 0; registerNumber < 8; registerNumber++)
         {
 #pragma warning disable CS8509
@@ -25,11 +27,13 @@ public partial class Instructions
             };
 #pragma warning restore CS8509
 
-            _instructions.Add(0xDDCB00 + registerNumber, new Instruction(d => RLC_aRRd_R(RegisterPair.IX, d, register), $"RLC (IX + d){(register == null ? string.Empty : $", {register}")}", 0xDDCB00 + registerNumber, 0));
+            _instructions.Add(0xDDCB00 + registerNumber, new Instruction(d => RLC_aRRd_R(RegisterPair.IX, d, register), $"RLC ({registerPair} + d){(register == null ? string.Empty : $", {register}")}", 0xDDCB00 + registerNumber, 0));
 
-            _instructions.Add(0xDDCB08 + registerNumber, new Instruction(d => RRC_aRRd_R(RegisterPair.IX, d, register), $"RRC (IX + d){(register == null ? string.Empty : $", {register}")}", 0xDDCB08 + registerNumber, 0));
+            _instructions.Add(0xDDCB08 + registerNumber, new Instruction(d => RRC_aRRd_R(RegisterPair.IX, d, register), $"RRC ({registerPair} + d){(register == null ? string.Empty : $", {register}")}", 0xDDCB08 + registerNumber, 0));
 
-            _instructions.Add(0xDDCB10 + registerNumber, new Instruction(d => RL_aRRd_R(RegisterPair.IX, d, register), $"RL (IX + d){(register == null ? string.Empty : $", {register}")}", 0xDDCB10 + registerNumber, 0));
+            _instructions.Add(0xDDCB10 + registerNumber, new Instruction(d => RL_aRRd_R(RegisterPair.IX, d, register), $"RL ({registerPair} + d){(register == null ? string.Empty : $", {register}")}", 0xDDCB10 + registerNumber, 0));
+
+            _instructions.Add(0xDDCB20 + registerNumber, new Instruction(d => RR_aRRd_R(RegisterPair.IX, d, register), $"RR ({registerPair} + d){(register == null ? string.Empty : $", {register}")}", 0xDDCB20 + registerNumber, 0));
         }
 
         for (var bit = 0; bit < 8; bit++)
@@ -52,11 +56,11 @@ public partial class Instructions
                 };
 #pragma warning restore CS8509
 
-                _instructions.Add(0xDDCB40 + registerNumber + bit * 8, new Instruction(d => BIT_b_aRRd((byte) (1 << innerBit), RegisterPair.IX, d), $"BIT {bit}, (IX + d)", 0xDDCB40 + registerNumber + bit * 8, 0));
+                _instructions.Add(0xDDCB40 + registerNumber + bit * 8, new Instruction(d => BIT_b_aRRd((byte) (1 << innerBit), RegisterPair.IX, d), $"BIT {bit}, ({registerPair} + d)", 0xDDCB40 + registerNumber + bit * 8, 0));
 
-                _instructions.Add(0xDDCB80 + registerNumber + bit * 8, new Instruction(d => RES_b_aRRd_R((byte) (1 << innerBit), RegisterPair.IX, d, register), $"RES {bit}, (IX + d){(register == null ? string.Empty : $", {register}")}", 0xDDCB80 + registerNumber + bit * 8, 0));
+                _instructions.Add(0xDDCB80 + registerNumber + bit * 8, new Instruction(d => RES_b_aRRd_R((byte) (1 << innerBit), RegisterPair.IX, d, register), $"RES {bit}, ({registerPair} + d){(register == null ? string.Empty : $", {register}")}", 0xDDCB80 + registerNumber + bit * 8, 0));
 
-                _instructions.Add(0xDDCBC0 + registerNumber + bit * 8, new Instruction(d => SET_b_aRRd_R((byte) (1 << innerBit), RegisterPair.IX, d, register), $"SET {bit}, (IX + d){(register == null ? string.Empty : $", {register}")}", 0xDDCBC0 + registerNumber + bit * 8, 0));
+                _instructions.Add(0xDDCBC0 + registerNumber + bit * 8, new Instruction(d => SET_b_aRRd_R((byte) (1 << innerBit), RegisterPair.IX, d, register), $"SET {bit}, (I{registerPair}X + d){(register == null ? string.Empty : $", {register}")}", 0xDDCBC0 + registerNumber + bit * 8, 0));
             }
         }
     }
