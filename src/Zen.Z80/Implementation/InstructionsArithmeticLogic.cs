46e9024537b78484440d6c6853fc393801ae70d1
Stevö <stevehuwjohn@gmail.com>
2023-04-28 21:45:55 +0100

    WIP

--- src/Zen.Z80/Implementation/InstructionsArithmeticLogic.cs
+++ src/Zen.Z80/Implementation/InstructionsArithmeticLogic.cs
@@ -321,6 +321,45 @@ public partial class Instructions
         _state.SetMCycles(4);
     }
 
+    private void DEC_R(Register register)
+    {
+        unchecked
+        {
+            var value = _state[register];
+
+            var result = (byte) (value - 1);
+
+            _state[register] = result;
+
+            // Carry unaffected
+            _state[Flag.AddSubtract] = true;
+            _state[Flag.ParityOverflow] = value == 0x80;
+            _state[Flag.X1] = (result & 0x08) > 0;
+            _state[Flag.HalfCarry] = (value & 0x0F) == 0;
+            _state[Flag.X2] = (result & 0x20) > 0;
+            _state[Flag.Zero] = result == 0;
+            _state[Flag.Sign] = (sbyte) result < 0;
+        }
+
+        _state.SetMCycles(4);
+    }
+
+    private void DEC_RR(RegisterPair registers)
+    {
+        unchecked
+        {
+            var value = _state[registers];
+
+            var result = (ushort) (value - 1);
+
+            _state[registers] = result;
+
+            _state.Q = 0;
+        }
+
+        _state.SetMCycles(4);
+    }
+
     private void INC_R(Register register)
     {
         unchecked
--- src/Zen.Z80/Implementation/Instructions_Initialise_Base.cs
+++ src/Zen.Z80/Implementation/Instructions_Initialise_Base.cs
@@ -70,8 +70,40 @@ public partial class Instructions
 
     private void InitialiseIncDecInstructions()
     {
-        _instructions.Add(0x03, new Instruction(_ => INC_RR(RegisterPair.BC), "INC BC", 0x04, 0));
+        _instructions.Add(0x03, new Instruction(_ => INC_RR(RegisterPair.BC), "INC BC", 0x03, 0));
 
         _instructions.Add(0x04, new Instruction(_ => INC_R(Register.B), "INC B", 0x04, 0));
+
+        _instructions.Add(0x05, new Instruction(_ => DEC_R(Register.B), "DEC B", 0x05, 0));
+
+        _instructions.Add(0x0B, new Instruction(_ => DEC_RR(RegisterPair.BC), "DEC BC", 0x0B, 0));
+
+        _instructions.Add(0x0C, new Instruction(_ => INC_R(Register.B), "INC C", 0x0C, 0));
+
+        _instructions.Add(0x0D, new Instruction(_ => DEC_R(Register.B), "DEC C", 0x0D, 0));
+
+        _instructions.Add(0x13, new Instruction(_ => INC_RR(RegisterPair.DE), "INC DE", 0x13, 0));
+
+        _instructions.Add(0x14, new Instruction(_ => INC_R(Register.D), "INC D", 0x14, 0));
+
+        _instructions.Add(0x15, new Instruction(_ => DEC_R(Register.D), "DEC D", 0x15, 0));
+
+        _instructions.Add(0x1B, new Instruction(_ => DEC_RR(RegisterPair.DE), "DEC DE", 0x1B, 0));
+
+        _instructions.Add(0x1C, new Instruction(_ => INC_R(Register.E), "INC E", 0x1C, 0));
+
+        _instructions.Add(0x1D, new Instruction(_ => DEC_R(Register.E), "DEC E", 0x1D, 0));
+
+        _instructions.Add(0x23, new Instruction(_ => INC_RR(RegisterPair.HL), "INC HL", 0x23, 0));
+
+        _instructions.Add(0x24, new Instruction(_ => INC_R(Register.H), "INC H", 0x24, 0));
+
+        _instructions.Add(0x25, new Instruction(_ => DEC_R(Register.H), "DEC H", 0x25, 0));
+
+        _instructions.Add(0x2B, new Instruction(_ => DEC_RR(RegisterPair.HL), "DEC HL", 0x2B, 0));
+
+        _instructions.Add(0x2C, new Instruction(_ => INC_R(Register.L), "INC L", 0x2C, 0));
+
+        _instructions.Add(0x2D, new Instruction(_ => DEC_R(Register.L), "DEC L", 0x2D, 0));
     }
 }
