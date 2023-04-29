a419891d01a778b838e7e82ff04104c1adf49cc2
Stevö <stevehuwjohn@gmail.com>
2023-04-29 13:18:27 +0100

    WIP

--- src/Zen.Z80/Implementation/InstructionsBranch.cs
+++ src/Zen.Z80/Implementation/InstructionsBranch.cs
@@ -13,11 +13,11 @@ public partial class Instructions
         {
             _state.StackPointer--;
 
-            _state.StackPointer = (byte) ((_state.ProgramCounter & 0xFF00) >> 8);
+            _interface.WriteToMemory(_state.StackPointer, (byte) ((_state.ProgramCounter & 0xFF00) >> 8));
 
             _state.StackPointer--;
 
-            _state.StackPointer = (byte) (_state.ProgramCounter & 0x00FF);
+            _interface.WriteToMemory(_state.StackPointer, (byte) (_state.ProgramCounter & 0x00FF));
 
             _state.ProgramCounter = (ushort) ((parameters[1] << 8) | parameters[0]);
 
