f883106c2cc008679a5306f471c6f6d480a984e4
Stevö <stevehuwjohn@gmail.com>
2023-04-30 13:46:04 +0100

    WIP

--- src/Zen.Z80/Implementation/InstructionsPort.cs
+++ src/Zen.Z80/Implementation/InstructionsPort.cs
@@ -180,12 +180,12 @@ public partial class Instructions
 
             _state[Register.B]--;
 
-            _state[Flag.Carry] = data + ((_state[Register.C] + 1) & 0xFF) > 0xFF;
-            _state[Flag.AddSubtract] = (data & 0x80) > 0;
-            _state[Flag.ParityOverflow] = ((ushort) (((data + ((_state[Register.C] + 1) & 0xFF)) & 0x07) ^ _state[Register.B])).IsEvenParity();
-            _state[Flag.X1] = (_state[Register.B] & 0x08) > 0;
-            _state[Flag.HalfCarry] = data + ((_state[Register.C] + 1) & 0xFF) > 0xFF;
-            _state[Flag.X2] = (_state[Register.B] & 0x20) > 0;
+            _state[Flag.Carry] = data > _state[Register.A];
+            _state[Flag.AddSubtract] = true;
+            _state[Flag.ParityOverflow] = _state[RegisterPair.BC] != 0;
+            _state[Flag.X1] = (data & 0x08) > 0;
+            _state[Flag.HalfCarry] = (_state[Register.A] & 0x0F) < (data & 0x0F);
+            _state[Flag.X2] = (data & 0x20) > 0;
             _state[Flag.Zero] = _state[Register.B] == 0;
             _state[Flag.Sign] = (sbyte) _state[Register.B] < 0;
 
