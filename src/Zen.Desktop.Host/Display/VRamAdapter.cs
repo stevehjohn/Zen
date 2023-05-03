9973563042ee91f39389282a564259498685e956
Stevö <stevehuwjohn@gmail.com>
2023-05-03 18:03:59 +0100

    WIP

--- src/Zen.Desktop.Host/Display/VRamAdapter.cs
+++ src/Zen.Desktop.Host/Display/VRamAdapter.cs
@@ -1,8 +1,6 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using System.Diagnostics;
-using Microsoft.Xna.Framework.Content;
-using Zen.System.Modules;
 using Color = Microsoft.Xna.Framework.Color;
 using Constants = Zen.Common.Constants;
 
@@ -10,7 +8,7 @@ namespace Zen.Desktop.Host.Display;
 
 public class VRamAdapter
 {
-    private readonly Ram _ram;
+    private readonly byte[] _screenFrame;
 
     private readonly GraphicsDeviceManager _graphicsDeviceManager;
 
@@ -22,9 +20,9 @@ public class VRamAdapter
 
     public Texture2D Display => _display;
 
-    public VRamAdapter(Ram ram, GraphicsDeviceManager graphicsDeviceManager)
+    public VRamAdapter(byte[] screenFrame, GraphicsDeviceManager graphicsDeviceManager)
     {
-        _ram = ram;
+        _screenFrame = screenFrame;
 
         _graphicsDeviceManager = graphicsDeviceManager;
 
@@ -37,45 +35,46 @@ public class VRamAdapter
        
         var data = new Color[Constants.ScreenWidthPixels * Constants.ScreenHeightPixels];
 
-        var i = 0;
+        for (var p = 0; p < 0xC000; p++)
+        {
+            data[p] = _screenFrame[p] == 0xFF ? Color.White : Color.Black;
+        }
 
         _alternate = _stopwatch.ElapsedMilliseconds > 500;
 
-        var screenRam = _ram.ScreenRam;
+        //for (var y = 0; y < Constants.ScreenHeightPixels; y++)
+        //{
+        //    for (var x = 0; x < Constants.ScreenWidthBytes; x++)
+        //    {
+        //        var address = 0;
 
-        for (var y = 0; y < Constants.ScreenHeightPixels; y++)
-        {
-            for (var x = 0; x < Constants.ScreenWidthBytes; x++)
-            {
-                var address = 0;
+        //        address |= (y & 0b0000_0111) << 8;
 
-                address |= (y & 0b0000_0111) << 8;
+        //        address |= (y & 0b1100_0000) << 5;
 
-                address |= (y & 0b1100_0000) << 5;
+        //        address |= (y & 0b0011_1000) << 2;
 
-                address |= (y & 0b0011_1000) << 2;
+        //        address |= x;
 
-                address |= x;
+        //        var segment = screenRam[address];
 
-                var segment = screenRam[address];
+        //        var colours = GetColours(x, y, screenRam);
 
-                var colours = GetColours(x, y, screenRam);
+        //        for (var b = 7; b >= 0; b--)
+        //        {
+        //            if ((segment & (0x01 << b)) > 0)
+        //            {
+        //                data[i] = colours.Foreground;
+        //            }
+        //            else
+        //            {
+        //                data[i] = colours.Background;
+        //            }
 
-                for (var b = 7; b >= 0; b--)
-                {
-                    if ((segment & (0x01 << b)) > 0)
-                    {
-                        data[i] = colours.Foreground;
-                    }
-                    else
-                    {
-                        data[i] = colours.Background;
-                    }
-
-                    i++;
-                }
-            }
-        }
+        //            i++;
+        //        }
+        //    }
+        //}
 
         texture.SetData(data);
 
