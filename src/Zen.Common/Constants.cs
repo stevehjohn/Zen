c20fcf72125e45633f54fb4c1516f4944178cd2a
Stevö <stevehuwjohn@gmail.com>
2023-05-08 15:46:33 +0100

    WIP

--- src/Zen.Common/Constants.cs
+++ src/Zen.Common/Constants.cs
@@ -2,7 +2,13 @@
 
 public static class Constants
 {
-    public const int ScreenWidthPixels = 256;
+    public const int ScreenWidthPixels = 288;
 
-    public const int ScreenHeightPixels = 192;
+    public const int ScreenHeightPixels = 224;
+
+    public const int PaperWidthPixels = 256;
+
+    public const int PaperHeightPixels = 192;
+
+    public const int BorderPixels = 16;
 }
