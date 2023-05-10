7ddfea84a0d5a1dad957860c25bd33370ce08130
Stevö <stevehuwjohn@gmail.com>
2023-05-10 16:09:26 +0100

    WIP

---
+++ src/Zen.Desktop.Host/Peripherals/Kempston.cs
@@ -0,0 +1,16 @@
+﻿using Zen.System.Interfaces;
+
+namespace Zen.Desktop.Host.Peripherals;
+
+public class Kempston : IPeripheral
+{
+    public byte? GetPortState(ushort port)
+    {
+        if ((port & 0xFF) is 0x1F or 0xDF)
+        {
+            return 0x00;
+        }
+
+        return 0xFF;
+    }
+}
