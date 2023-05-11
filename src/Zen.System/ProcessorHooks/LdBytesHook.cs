85a9687bfb4d1a1ddbfea51a30d15f80f4bbce9f
Stevö <stevehuwjohn@gmail.com>
2023-05-11 18:10:06 +0100

    WIP

--- src/Zen.System/ProcessorHooks/LdBytesHook.cs
+++ src/Zen.System/ProcessorHooks/LdBytesHook.cs
@@ -5,16 +5,76 @@ namespace Zen.System.ProcessorHooks;
 
 public class LdBytesHook : IProcessorHook
 {
+    private const int PauseLength = 30;
+
+    private byte[] _data = Array.Empty<byte>();
+
+    private int _position;
+
+    private int _bit;
+
+    private int _pause;
+
     public bool Activate(State state)
     {
-        return state.ProgramCounter == 0x0556;
+        if (state.ProgramCounter == 0x0556)
+        {
+            _data = File.ReadAllBytes("C:\\Users\\steve\\Downloads\\Aqua Plane.tap");
+            
+            _data = _data[3..20];
+
+            _position = 0;
+
+            _bit = 0b0000_0001;
+
+            _pause = 0;
+
+            return true;
+        }
+
+        return false;
     }
 
     public bool ExecuteCycle(State state, Interface @interface)
     {
-        Ret(state, @interface);
+        if (_pause > 0)
+        {
+            _pause--;
 
-        return true;
+            return false;
+        }
+
+        _pause = PauseLength;
+
+        @interface.WriteToPort(0xFE, (byte) ((_data[_position] & _bit) > 0 ? 5 : 6));
+
+        _bit <<= 1;
+
+        if (_bit == 0x0100)
+        {
+            @interface.WriteToMemory(state[RegisterPair.IX], _data[_position]);
+
+            state[RegisterPair.IX]++;
+
+            state[RegisterPair.DE]--;
+
+            if (state[RegisterPair.DE] == 0)
+            {
+                Ret(state, @interface);
+
+                state[Flag.Carry] = true;
+
+                @interface.WriteToPort(0xFE, 7);
+
+                return true;
+            }
+
+            _bit = 1;
+
+            _position++;
+        }
+
+        return false;
     }
 
     private static void Ret(State state, Interface @interface)
