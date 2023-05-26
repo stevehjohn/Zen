3103d09a0b74b5b6aabc40e912a49b43e8a42cb4
Stevö <stevehuwjohn@gmail.com>
2023-05-26 17:41:14 +0100

    WIP

---
+++ src/Zen.System/Modules/Audio/Channel.cs
@@ -0,0 +1,75 @@
+﻿namespace Zen.System.Modules.Audio;
+
+public class Channel
+{
+    public ushort TonePeriod
+    {
+        set => _toneGenerator.Period = value;
+        get => _toneGenerator.Period;
+    }
+
+    public byte Volume
+    {
+        set
+        {
+            _toneGenerator.Volume = value;
+            _noiseGenerator.Volume = value;
+        }
+    }
+
+    public ushort EnvelopePeriod
+    {
+        set
+        {
+            _toneGenerator.EnvelopePeriod = value;
+            _noiseGenerator.EnvelopePeriod = value;
+        }
+        get => _toneGenerator.EnvelopePeriod;
+    }
+
+    public byte Envelope
+    {
+        set
+        {
+            _toneGenerator.Envelope = value;
+            _noiseGenerator.Envelope = value;
+        }
+    }
+
+
+    public bool EnvelopeOn
+    {
+        set
+        {
+            _toneGenerator.EnvelopeOn = value;
+            _noiseGenerator.EnvelopeOn = value;
+        }
+    }
+
+
+    public byte NoisePeriod
+    {
+        set => _noiseGenerator.Period = value;
+    }
+
+    private readonly SignalGenerator _toneGenerator;
+
+    private readonly SignalGenerator _noiseGenerator;
+
+    public Channel()
+    {
+        _toneGenerator = new SignalGenerator(false);
+
+        _noiseGenerator = new SignalGenerator(true);
+    }
+
+    public float GetNextToneSignal()
+    {
+        return _toneGenerator.GetNextSignal();
+    }
+
+    public float GetNextNoiseSignal()
+    {
+        return _noiseGenerator.GetNextSignal();
+    }
+}
---
+++ src/Zen.System/Modules/Audio/Constants.cs
@@ -0,0 +1,14 @@
+﻿namespace Zen.System.Modules.Audio;
+
+public class Constants
+{
+    public const int SampleRate = 44100;
+
+    public const int Channels = 3;
+
+    public const int BufferSize = SampleRate / 50;
+
+    public const float Amplitude = 0.35f * short.MaxValue;
+
+    public const float AyFrequency = 1773500f;
+}
