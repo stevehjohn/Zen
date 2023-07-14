4239e4517d8072a24ce84a0605c42a04f029a962
Stevö <stevehuwjohn@gmail.com>
2023-07-14 17:44:34 +0100

    WIP

--- src/Zen.System/Modules/Audio/SignalGenerator.cs
+++ src/Zen.System/Modules/Audio/SignalGenerator.cs
@@ -138,7 +138,7 @@ public class SignalGenerator
 
         if (! EnvelopeOn)
         {
-            signal *= Volume / 15f * Constants.Amplitude;
+            signal *= NormaliseVolume(Volume) * Constants.Amplitude;
         }
         else
         {
@@ -148,6 +148,31 @@ public class SignalGenerator
         }
 
         return signal;
+    }
+
+    private static float NormaliseVolume(byte volume)
+    {
+        var result = volume switch
+        {
+            1 => 0.0105f,
+            2 => 0.0154f,
+            3 => 0.0216f,
+            4 => 0.0314f,
+            5 => 0.0461f,
+            6 => 0.0635f,
+            7 => 0.1061f,
+            8 => 0.1319f,
+            9 => 0.2163f,
+            10 => 0.2973f,
+            11 => 0.3908f,
+            12 => 0.5129f,
+            13 => 0.6371f,
+            14 => 0.8186f,
+            15 => 1.0000f,
+            _ => 0f
+        };
+
+        return result;
     }
 
     private void CycleEnvelope()
