a8d381e9f61aa8327109c7bfdc96ff3c32ce3251
Stevö <stevehuwjohn@gmail.com>
2023-07-26 14:28:04 +0100

    LSFR

---
+++ src/Zen.System/Modules/Audio/Lsfr.cs
@@ -0,0 +1,21 @@
+﻿namespace Zen.System.Modules.Audio;
+
+// ReSharper disable  IdentifierTypo
+public static class Lsfr
+{
+    private static uint _seed = 1;
+
+    public static float Value { get; private set; }
+
+    public static void GenerateNextValue()
+    {
+        if ((_seed & 1) > 0)
+        {
+            _seed ^= 0x24000;
+        }
+
+        _seed >>= 1;
+
+        Value = _seed / 131_070f;
+    }
+}
