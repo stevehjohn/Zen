cdb1a93fc0ef18b0ba311bf34d066046655afd3e
Stevo John <stevehuwjohn@gmail.com>
2023-07-26 10:50:18 +0100

    WIP

--- src/Zen.Desktop.Host/Infrastructure/Host.cs
+++ src/Zen.Desktop.Host/Infrastructure/Host.cs
@@ -265,6 +265,8 @@ public class Host : Game
         {
             _graphicsDeviceManager.PreferredBackBufferWidth = (Constants.ScreenWidthPixels + Constants.WavePanelWidth) * _scaleFactor;
             _graphicsDeviceManager.PreferredBackBufferHeight = Constants.ScreenHeightPixels * _scaleFactor;
+
+            _waveVisualiser.ScaleFactor = scale;
         }
         else
         {
