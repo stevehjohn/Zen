b514f391a676bbcdaab787949239e88d049b9505
Stevö <stevehuwjohn@gmail.com>
2023-07-27 13:43:00 +0100

    WIP

--- src/Zen.Desktop.Host/Features/CountersVisualiser.cs
+++ src/Zen.Desktop.Host/Features/CountersVisualiser.cs
@@ -1,8 +1,10 @@
-﻿using System;
-using Microsoft.Xna.Framework;
+﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Content;
 using Microsoft.Xna.Framework.Graphics;
+using System;
 using Zen.Common;
+using Zen.Common.Infrastructure;
+using Zen.Desktop.Host.Infrastructure;
 
 namespace Zen.Desktop.Host.Features;
 
@@ -16,11 +18,9 @@ public class CountersVisualiser
 
     private readonly ContentManager _contentManager;
 
-    private Texture2D _texture;
+    private readonly Texture2D _texture;
 
-    public int ScaleFactor { get; set; }
-
-    public CountersVisualiser(GraphicsDeviceManager graphicsDeviceManager, ContentManager contentManager, int scaleFactor)
+    public CountersVisualiser(GraphicsDeviceManager graphicsDeviceManager, ContentManager contentManager)
     {
         _graphicsDeviceManager = graphicsDeviceManager;
         
@@ -32,19 +32,59 @@ public class CountersVisualiser
 
         characterSet.GetData(_characterSet);
 
-        ScaleFactor = scaleFactor;
+        _data = new Color[Constants.ScreenWidthPixels * Constants.CountersPanelHeight];
 
-        _data = new Color[Constants.WavePanelWidth * ScaleFactor * Constants.CountersPanelHeight * ScaleFactor];
-
-        _texture = new Texture2D(_graphicsDeviceManager.GraphicsDevice, Constants.WavePanelWidth * ScaleFactor, Constants.CountersPanelHeight * ScaleFactor);
+        _texture = new Texture2D(_graphicsDeviceManager.GraphicsDevice, Constants.ScreenWidthPixels, Constants.CountersPanelHeight);
     }
 
     public Texture2D RenderPanel()
     {
         Array.Fill(_data, Color.Black);
 
+        DrawString("FPS", Color.Magenta, 2, 0);
+        DrawString(":", Color.White, 5, 0);
+        DrawString(Counters.Instance.GetCountPerSecond(Counter.RenderedFrames).ToString(), Color.Cyan, 7, 0);
+
+        DrawString("Z80Ops/s", Color.Magenta, 15, 0);
+        DrawString(":", Color.White, 23, 0);
+        DrawString(Counters.Instance.GetCountPerSecond(Counter.Instructions).ToString(), Color.Cyan, 25, 0);
+
+        DrawString("Hz", Color.Magenta, 2, 1);
+        DrawString(":", Color.White, 5, 1);
+        DrawString(Counters.Instance.GetCountPerSecond(Counter.SpectrumFrames).ToString(), Color.Cyan, 7, 1);
+
+        DrawString("AYFPS", Color.Magenta, 15, 1);
+        DrawString(":", Color.White, 23, 1);
+        DrawString(Counters.Instance.GetCountPerSecond(Counter.AyFrames).ToString(), Color.Cyan, 25, 1);
+
         _texture.SetData(_data);
 
         return _texture;
+    }
+
+    private void DrawString(string text, Color color, int x, int y)
+    {
+        for (var i = 0; i < text.Length; i++)
+        {
+            DrawCharacter(text[i], color, x + i, y);
+        }
+    }
+
+    private void DrawCharacter(char c, Color color, int x, int y)
+    {
+        var co = CharacterMap.Instance.GetCharacterStartPixel(c);
+
+        for (var bY = 0; bY < 8; bY++)
+        {
+            for (var bX = 0; bX < 8; bX++)
+            {
+                if (_characterSet[bY * 128 + bX + co].A == 0)
+                {
+                    continue;
+                }
+
+                _data[x * 8 + y * 8 * Constants.ScreenWidthPixels + bX + bY * Constants.ScreenWidthPixels] = color;
+            }
+        }
     }
 }
