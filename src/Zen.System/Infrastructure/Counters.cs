76f6c2bb64bcfb3109727a44eb33727540bf6c35
Stevö <stevehuwjohn@gmail.com>
2023-07-27 11:46:50 +0100

    WIP

---
+++ src/Zen.System/Infrastructure/Counters.cs
@@ -0,0 +1,52 @@
+﻿using System.Diagnostics;
+
+namespace Zen.System.Infrastructure;
+
+public class Counters
+{
+    private readonly ulong[] _totalCounts;
+
+    private readonly ulong[] _temporaryCountsPerSecond;
+
+    private readonly ulong[] _countsPerSecond;
+
+    private readonly Stopwatch _stopwatch;
+
+    public static Counters Instance { get; } = new ();
+
+    private Counters()
+    {
+        var numberOfCounters = Enum.GetNames<Counter>().Length;
+
+        _totalCounts = new ulong[numberOfCounters];
+
+        _temporaryCountsPerSecond = new ulong[numberOfCounters];
+
+        _countsPerSecond = new ulong[numberOfCounters];
+
+        _stopwatch = Stopwatch.StartNew();
+    }
+
+    public void IncrementCounter(Counter counter, int value)
+    {
+        _totalCounts[(int) counter] += (ulong) value;
+
+        if (_stopwatch.ElapsedMilliseconds > 1000)
+        {
+            _stopwatch.Restart();
+
+            _countsPerSecond[(int) counter] = _temporaryCountsPerSecond[(int) counter];
+
+            _temporaryCountsPerSecond[(int) counter] = (ulong) value;
+        }
+        else
+        {
+            _temporaryCountsPerSecond[(int) counter] += (ulong) value;
+        }
+    }
+
+    public ulong GetCounterPerSecond(Counter counter)
+    {
+        return _countsPerSecond[(int) counter];
+    }
+}
