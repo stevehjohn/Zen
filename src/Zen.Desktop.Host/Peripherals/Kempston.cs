10fe97c47ddd1526de43eef19ab09389250c60d1
Stevö <stevehuwjohn@gmail.com>
2023-07-27 19:29:33 +0100

    WIP

--- src/Zen.Desktop.Host/Peripherals/Kempston.cs
+++ src/Zen.Desktop.Host/Peripherals/Kempston.cs
@@ -1,4 +1,5 @@
-﻿using Zen.System.Interfaces;
+﻿using Microsoft.Xna.Framework.Input;
+using Zen.System.Interfaces;
 
 namespace Zen.Desktop.Host.Peripherals;
 
@@ -8,9 +9,47 @@ public class Kempston : IPeripheral
     {
         if ((port & 0xFF) is 0x1F or 0xDF)
         {
-            return 0x00;
+            return MapKeysToJoystick();
         }
 
         return null;
+    }
+
+    private byte MapKeysToJoystick()
+    {
+        var keys = Microsoft.Xna.Framework.Input.Keyboard.GetState().GetPressedKeys();
+
+        // 000FUDLR
+        var data = (byte) 0;
+
+        foreach (var key in keys)
+        {
+            if (key == Keys.O)
+            {
+                data |= 0b0000_0010;
+            }
+
+            if (key == Keys.P)
+            {
+                data |= 0b0000_0001;
+            }
+
+            if (key == Keys.Q)
+            {
+                data |= 0b0000_1000;
+            }
+
+            if (key == Keys.A)
+            {
+                data |= 0b0000_0100;
+            }
+
+            if (key == Keys.M)
+            {
+                data |= 0b0001_0000;
+            }
+        }
+
+        return data;
     }
 }
