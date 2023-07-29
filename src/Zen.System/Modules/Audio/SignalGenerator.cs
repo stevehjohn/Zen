fa0a7c441f28fd3b58d6039c6f5ba30fd438d0af
Stevö <stevehuwjohn@gmail.com>
2023-07-29 12:30:36 +0100

    Clean up

--- src/Zen.System/Modules/Audio/SignalGenerator.cs
+++ src/Zen.System/Modules/Audio/SignalGenerator.cs
@@ -10,12 +10,6 @@ public class SignalGenerator
 
     private float _noiseValue;
 
-    private float _lastValue;
-
-    private float _delta;
-
-    private int _valueCycles;
-
     public SignalGenerator(bool noise)
     {
         _noise = noise;
@@ -45,65 +39,6 @@ public class SignalGenerator
 
         var signal = _noise ? _noiseValue : (float) Math.Sin(_phaseAngle);
 
-        signal = SquareWave(signal);
-
         return signal;
-    }
-
-    private float SquareWave(float sineValue)
-    {
-        if (sineValue > 0)
-        {
-            if (_lastValue <= 0)
-            {
-                _lastValue = 1;
-
-                _delta = 0.05f;
-            }
-            else
-            {
-                _lastValue -= _delta;
-
-                _delta /= 1.2f;
-            }
-
-            return _lastValue;
-        }
-
-        if (sineValue < 0)
-        {
-            if (_lastValue >= 0)
-            {
-                _lastValue = -1;
-
-                _delta = 0.05f;
-            }
-            else
-            {
-                _lastValue += _delta;
-
-                _delta /= 1.2f;
-            }
-
-            return _lastValue;
-        }
-
-        return 0;
-    }
-
-    private float GetValueForCycle(float sineValue)
-    {
-        var value = _valueCycles switch
-        {
-            >= 0 and < 5 => 1f,
-            >= 5 and < 10 => 0.95f,
-            >= 10 and < 20 => 0.9f,
-            >= 20 and < 40 => 0.85f,
-            _ => 0.875f
-        };
-
-        _valueCycles++;
-
-        return sineValue < 0 ? -value : value;
     }
 }
