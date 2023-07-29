0f6a9f606ea9a7f0cc17e371c3cc7a17142195b9
Stevö <stevehuwjohn@gmail.com>
2023-07-29 21:26:11 +0100

    WIP

---
+++ src/Zen.System/Modules/Audio/EnvelopeGenerator.cs
@@ -0,0 +1,30 @@
+﻿namespace Zen.System.Modules.Audio;
+
+public class EnvelopeGenerator
+{
+    public byte FinePeriod 
+    {
+        set
+        {
+            _period = (_period & 0xFF00) | value;
+
+            RecalculateParameters();
+        }
+    }
+
+    public byte CoarsePeriod 
+    {
+        set
+        {
+            _period = (_period & 0x00FF) | (value << 8);
+
+            RecalculateParameters();
+        }
+    }
+
+    private int _period;
+
+    private void RecalculateParameters()
+    {
+    }
+}
