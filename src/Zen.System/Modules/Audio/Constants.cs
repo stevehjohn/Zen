56d50df3c667cf3fc78ff469753ba1a034786319
Stevö <stevehuwjohn@gmail.com>
2023-07-31 13:26:36 +0100

    WIP

--- src/Zen.System/Modules/Audio/Constants.cs
+++ src/Zen.System/Modules/Audio/Constants.cs
@@ -4,8 +4,6 @@ public class Constants
 {
     public const int SampleRate = 44_100;
 
-    public const int Channels = 3;
-
     public const int BufferSize = SampleRate / Common.Constants.SpectrumFramesPerSecond;
 
     public const float ChannelAmplitude = 0.33f;
