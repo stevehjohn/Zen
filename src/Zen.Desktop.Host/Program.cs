dc72fa703fb93401608b75e7f6df53641a6e641e
Stevö <stevehuwjohn@gmail.com>
2023-08-02 16:53:11 +0100

    more logging.

--- src/Zen.Desktop.Host/Program.cs
+++ src/Zen.Desktop.Host/Program.cs
@@ -1,11 +1,21 @@
-﻿namespace Zen.Desktop.Host;
+﻿using System;
+using Zen.Common.Infrastructure;
+
+namespace Zen.Desktop.Host;
 
 public static class Program
 {
     public static void Main()
     {
-        using var monitor = new Infrastructure.Host();
+        try
+        {
+            using var host = new Infrastructure.Host();
 
-        monitor.Run();
+            host.Run();
+        }
+        catch (Exception exception)
+        {
+            Logger.LogException(nameof(Program), exception);
+        }
     }
 }
