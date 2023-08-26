d8e47a56268099ba6b84aadc69c37f5f0df8bfa9
Stevö <stevehuwjohn@gmail.com>
2023-08-26 17:51:48 +0100

    WIP

---
+++ src/Zen.Utilities/Infrastructure/OpCodeImplementationInspector.cs
@@ -0,0 +1,109 @@
+﻿using System.Text;
+
+namespace Zen.Utilities.Infrastructure;
+
+public class OpCodeImplementationInspector
+{
+    private readonly string _initialisationCode = LoadInitialisationCode();
+
+    private readonly string _methodCode = LoadMethodCode();
+
+    public string GetOpCodeImplementation(int opCode)
+    {
+        var method = GetCalledMethod(opCode);
+
+        var code = GetMethodCode(method);
+
+        return code;
+    }
+
+    private static string LoadInitialisationCode()
+    {
+        var files = Directory.EnumerateFiles("../../../../../src/Zen.Z80/Implementation", "Instructions_Initialise_*");
+
+        var data = new StringBuilder();
+
+        foreach (var file in files)
+        {
+            data.Append(File.ReadAllText(file));
+
+            data.AppendLine();
+        }
+
+        return data.ToString();
+    }
+
+    private static string LoadMethodCode()
+    {
+        var files = Directory.EnumerateFiles("../../../../../src/Zen.Z80/Implementation", "Instructions*");
+
+        var data = new StringBuilder();
+
+        foreach (var file in files)
+        {
+            if (! char.IsAsciiLetter(Path.GetFileName(file).Replace("Instructions", string.Empty)[0]))
+            {
+                continue;
+            }
+
+            data.Append(File.ReadAllText(file));
+
+            data.AppendLine();
+        }
+
+        return data.ToString();
+    }
+
+    private string GetCalledMethod(int opCode)
+    {
+        var initialisationIndex = _initialisationCode.IndexOf($"0x{opCode:X6}", StringComparison.InvariantCultureIgnoreCase);
+
+        var lambdaIndex = _initialisationCode.IndexOf("=>", initialisationIndex, StringComparison.InvariantCultureIgnoreCase);
+
+        var methodNameEnd = _initialisationCode.IndexOf("(", lambdaIndex, StringComparison.InvariantCultureIgnoreCase);
+
+        var methodString = _initialisationCode.Substring(lambdaIndex, methodNameEnd - lambdaIndex);
+
+        methodString = methodString.Replace("=>", string.Empty);
+
+        return methodString.Trim();
+    }
+
+    private string GetMethodCode(string methodName)
+    {
+        var index = _methodCode.IndexOf(methodName, StringComparison.InvariantCulture);
+
+        var braceCount = -1;
+
+        var code = new StringBuilder();
+        
+        while (braceCount != 0)
+        {
+            var character = _methodCode[index];
+
+            if (character == '{')
+            {
+                if (braceCount == -1)
+                {
+                    braceCount = 1;
+                }
+                else
+                {
+                    braceCount++;
+                }
+            }
+            else if (character == '}')
+            {
+                braceCount--;
+            }
+
+            code.Append(character);
+
+            index++;
+        }
+
+        code.Insert(0, "    public void ");
+
+        return code.ToString();
+    }
+}
