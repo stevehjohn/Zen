6c1ec491721fa5cf583c7965ecb1d5d03325940a
Stevo <stevehuwjohn@gmail.com>
2024-01-31 14:10:41 +0000

    wip

--- src/Zen.Desktop.Host/ProcessorHooks/ManicMiner/WillyBot.cs
+++ src/Zen.Desktop.Host/ProcessorHooks/ManicMiner/WillyBot.cs
@@ -55,13 +55,13 @@ public class WillyBot : IProcessorHook
             
             case 0x8C2F:
                 // Left, right
-                state[Register.A] = _direction;
+                //state[Register.A] = _direction;
                 
                 break;
             
             case 0x8C77:
                 // Jump
-                state[Register.A] = (byte) (_jump ? 16 : 0);
+                //state[Register.A] = (byte) (_jump ? 16 : 0);
                 
                 break;
             
