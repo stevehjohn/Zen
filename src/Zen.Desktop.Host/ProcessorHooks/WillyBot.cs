f1c0cd860718ec0f51901c5f6a84f84556854fec
Stevo <stevehuwjohn@gmail.com>
2024-01-31 11:02:58 +0000

    wip

--- src/Zen.Desktop.Host/ProcessorHooks/WillyBot.cs
+++ src/Zen.Desktop.Host/ProcessorHooks/WillyBot.cs
@@ -5,6 +5,10 @@ namespace Zen.Desktop.Host.ProcessorHooks;
 
 public class WillyBot : IProcessorHook
 {
+    private int _level;
+
+    private int _iteration;
+    
     public bool Activate(State state)
     {
         return false;
@@ -26,6 +30,9 @@ public class WillyBot : IProcessorHook
             
             case 0x9028:
                 // Level complete
+                _level++;
+
+                _iteration = 0;
                 
                 break;
             
@@ -47,14 +54,30 @@ public class WillyBot : IProcessorHook
                 break;
             
             case 0x9312:
+                // Start game
+                _level = 1;
+
+                _iteration = 0;
+
                 state[Flag.Zero] = false;
 
                 break;
             
             case 0x8938:
+                // Infinite lives
                 @interface.WriteToMemory(0x8457, 3);
                 
                 break;
+            
+            case 0x870E:
+                // Main loop
+                GenerateNextMove();
+                
+                break;
         }
+    }
+
+    private void GenerateNextMove()
+    {
     }
 }
