7d1480e86265ed1c25fabacd2091f051d9f0348a
Stevo John <stevehuwjohn@gmail.com>
2024-02-01 13:39:42 +0000

    wip

--- src/Zen.Desktop.Host/ProcessorHooks/ManicMiner/WillyBot.cs
+++ src/Zen.Desktop.Host/ProcessorHooks/ManicMiner/WillyBot.cs
@@ -6,6 +6,8 @@ namespace Zen.Desktop.Host.ProcessorHooks.ManicMiner;
 public class WillyBot : IProcessorHook
 {
     private int _level;
+
+    private RoutePlanner _routePlanner;
     
     public bool Activate(State state)
     {
@@ -93,6 +95,10 @@ public class WillyBot : IProcessorHook
 
     private void StartLevel(Interface @interface)
     {
+        _routePlanner = new RoutePlanner(_level, @interface);
+        
+        _routePlanner.PlanRoute();
+        
         RestartLevel(@interface);
     }
 
