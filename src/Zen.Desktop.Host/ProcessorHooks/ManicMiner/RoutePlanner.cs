225fe97d211976540d8158aabea76ce9728b29d8
Stevo John <stevehuwjohn@gmail.com>
2024-02-01 14:34:38 +0000

    wip

--- src/Zen.Desktop.Host/ProcessorHooks/ManicMiner/RoutePlanner.cs
+++ src/Zen.Desktop.Host/ProcessorHooks/ManicMiner/RoutePlanner.cs
@@ -14,18 +14,30 @@ public class RoutePlanner
 
     public List<Move> GetRoute()
     {
-        var queue = new PriorityQueue<(int X, int Y), int>();
+        var queue = new PriorityQueue<(int X, int Y, List<Move> Moves, int Steps), int>();
         
-        queue.Enqueue((_levelData.Start.X * 8, _levelData.Start.Y * 8), 0);
+        queue.Enqueue((_levelData.Start.X * 8, _levelData.Start.Y * 8, [], 0), 0);
 
-        while (queue.TryDequeue(out var position, out _))
+        while (queue.TryDequeue(out var node, out _))
         {
+            var moves = GetMoves(node.X, node.Y);
+
+            foreach (var move in moves)
+            {
+                queue.Enqueue((move.X, move.Y, [..node.Moves, move.Move], node.Steps + 1), node.Steps + 1);
+            }
         }
 
         return null;
     }
 
-    private void GetMoves(int x, int y)
+    private List<(Move Move, int X, int Y)> GetMoves(int x, int y)
     {
+        return null;
+    }
+
+    private bool IsMoveSafe(int x, int y)
+    {
+        return false;
     }
 }
