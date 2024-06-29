2470538de314b8cdd85a641763175d1c21e81166
Steve John <steve.john@justeattakeaway.com>
2024-06-29 14:28:27 +0100

    WIP

--- src/Zen.Desktop.Host/Display/VideoRenderer.cs
+++ src/Zen.Desktop.Host/Display/VideoRenderer.cs
@@ -37,7 +37,7 @@ public class VideoRenderer
     {
         for (var p = 0; p < Constants.ScreenWidthPixels * Constants.ScreenHeightPixels; p++)
         {
-            _data[p] = GetColor(_screenFrame[p]);
+            _data[p] = GetC64Color(_screenFrame[p]);
         }
 
         _display.SetData(_data);
@@ -58,8 +58,8 @@ public class VideoRenderer
         var flash = (pixel & 0b0000_0010_0000_0000) > 0;
 
         var color = (flash && _flash) ^ ((pixel & 01000_0000_0000_0000) > 0)
-                        ? pixel & 0b0000_0111
-                        : (pixel & 0b0011_1000) >> 3;
+            ? pixel & 0b0000_0111
+            : (pixel & 0b0011_1000) >> 3;
 
         var intensity = (pixel & 0b0000_0001_0000_0000) > 0 ? 0xFF : 0xD8;
         
@@ -72,6 +72,29 @@ public class VideoRenderer
             5 => Color.FromNonPremultiplied(0, intensity, intensity, 255),
             6 => Color.FromNonPremultiplied(intensity, intensity, 0, 255),
             7 => Color.FromNonPremultiplied(intensity, intensity, intensity, 255),
+            _ => Color.FromNonPremultiplied(0, 0, 0, 255)
+        };
+    }
+
+    private Color GetC64Color(ushort pixel)
+    {
+        var flash = (pixel & 0b0000_0010_0000_0000) > 0;
+
+        var color = (flash && _flash) ^ ((pixel & 01000_0000_0000_0000) > 0)
+            ? pixel & 0b0000_0111
+            : (pixel & 0b0011_1000) >> 3;
+
+        var intensity = (pixel & 0b0000_0001_0000_0000) > 0;
+        
+        return color switch
+        {
+            1 => Color.FromNonPremultiplied(0, intensity ? 136 : 0, intensity ? 255 : 170, 255),
+            2 => Color.FromNonPremultiplied(intensity ? 255 : 136, intensity ? 119 : 0,  intensity ? 119 : 0, 255),
+            3 => Color.FromNonPremultiplied(204, 68, 204, 255),
+            4 => Color.FromNonPremultiplied(intensity ? 170 : 0, intensity ? 255 : 204, intensity ? 102 : 85, 255),
+            5 => Color.FromNonPremultiplied(0, 255, 255, 255),
+            6 => Color.FromNonPremultiplied(intensity ? 221 : 102, intensity ? 136 : 68, intensity ? 85 : 0, 255),
+            7 => Color.FromNonPremultiplied(intensity ? 255 : 187, intensity ? 255 : 187, intensity ? 255 : 187, 255),
             _ => Color.FromNonPremultiplied(0, 0, 0, 255)
         };
     }
