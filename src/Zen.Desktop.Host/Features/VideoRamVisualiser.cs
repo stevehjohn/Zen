3b7a399fb0ca2a15c5cc93b3fa5e5227c8594c33
Stevo John <stevehuwjohn@gmail.com>
2024-08-08 13:48:54 +0100

    WIP

--- src/Zen.Desktop.Host/Features/VideoRamVisualiser.cs
+++ src/Zen.Desktop.Host/Features/VideoRamVisualiser.cs
@@ -1,6 +1,34 @@
+using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Zen.Common;
+
 namespace Zen.Desktop.Host.Features;
 
 public class VideoRamVisualiser
 {
-    
+    // ReSharper disable once PrivateFieldCanBeConvertedToLocalVariable
+    private readonly GraphicsDeviceManager _graphicsDeviceManager;
+
+    private readonly Color[] _data;
+
+    private readonly Texture2D _ram;
+
+    public Texture2D Ram => _ram;
+
+    public VideoRamVisualiser(GraphicsDeviceManager graphicsDeviceManager)
+    {
+        _graphicsDeviceManager = graphicsDeviceManager;
+
+        _data = new Color[Constants.VideoRamVisualisationPanelWidth * Constants.ScreenHeightPixels];
+
+        _ram = new Texture2D(_graphicsDeviceManager.GraphicsDevice, Constants.VideoRamVisualisationPanelWidth, Constants.ScreenHeightPixels);
+    }
+
+    private void RenderRam()
+    {
+        Array.Fill(_data, Color.Black);
+
+        _ram.SetData(_data);
+    }
 }
