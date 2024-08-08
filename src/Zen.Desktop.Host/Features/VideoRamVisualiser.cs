44eb0a136c3f5fffb7322ae92eef7d62511f3766
Stevo John <stevehuwjohn@gmail.com>
2024-08-08 14:09:06 +0100

    WIP

--- src/Zen.Desktop.Host/Features/VideoRamVisualiser.cs
+++ src/Zen.Desktop.Host/Features/VideoRamVisualiser.cs
@@ -2,11 +2,14 @@ using System;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Zen.Common;
+using Zen.System.Modules;
 
 namespace Zen.Desktop.Host.Features;
 
 public class VideoRamVisualiser
 {
+    private const int YStart = Constants.ScreenWidthPixels * Constants.BorderPixels;
+    
     // ReSharper disable once PrivateFieldCanBeConvertedToLocalVariable
     private readonly GraphicsDeviceManager _graphicsDeviceManager;
 
@@ -14,7 +17,7 @@ public class VideoRamVisualiser
 
     private readonly Texture2D _ram;
 
-    public VideoRamVisualiser(GraphicsDeviceManager graphicsDeviceManager)
+    public VideoRamVisualiser(GraphicsDeviceManager graphicsDeviceManager, Ram ram)
     {
         _graphicsDeviceManager = graphicsDeviceManager;
 
@@ -26,6 +29,14 @@ public class VideoRamVisualiser
     public Texture2D RenderRam()
     {
         Array.Fill(_data, Color.Black);
+
+        for (var y = 0; y < Constants.PaperHeightPixels; y++)
+        {
+            for (var x = 0; x < Constants.PaperWidthPixels; x++)
+            {
+                _data[YStart + y * Constants.ScreenWidthPixels + Constants.BorderPixels + x] = Color.Aqua;
+            }
+        }
 
         _ram.SetData(_data);
 
