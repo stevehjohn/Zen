3d831b65da2fccc8c1c01aec745450f21a9afa92
Stevo John <stevehuwjohn@gmail.com>
2024-08-10 20:23:06 +0100

    WIP

--- src/Zen.Desktop.Host/Features/SpectrumAnalyser.cs
+++ src/Zen.Desktop.Host/Features/SpectrumAnalyser.cs
@@ -1,3 +1,6 @@
+using System;
+using System.Numerics;
+using MathNet.Numerics.IntegralTransforms;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Zen.Common;
@@ -12,7 +15,7 @@ public class SpectrumAnalyser
 
     private readonly Color[] _data;
 
-    private readonly float[][] _buffers;
+    private readonly Complex[][] _buffers;
     
     private readonly Texture2D _spectrum;
 
@@ -30,11 +33,11 @@ public class SpectrumAnalyser
 
         _spectrum = new Texture2D(_graphicsDeviceManager.GraphicsDevice, Constants.WaveVisualisationPanelWidth, Constants.ScreenHeightPixels);
 
-        _buffers = new float[4][];
+        _buffers = new Complex[4][];
 
         for (var i = 0; i < 4; i++)
         {
-            _buffers[i] = new float[BufferSize];
+            _buffers[i] = new Complex[BufferSize];
         }
     }
 
@@ -45,9 +48,9 @@ public class SpectrumAnalyser
             return;
         }
 
-        _buffers[0][_bufferPosition] = signals[0];
-        _buffers[1][_bufferPosition] = signals[1];
-        _buffers[2][_bufferPosition] = signals[2];
+        _buffers[0][_bufferPosition] = new Complex(signals[0], 0);
+        _buffers[1][_bufferPosition] = new Complex(signals[1], 0);
+        _buffers[2][_bufferPosition] = new Complex(signals[2], 0);
 
         _bufferPosition++;
 
@@ -64,5 +67,39 @@ public class SpectrumAnalyser
 
     private void RenderSpectrum()
     {
+        _rendering = true;
+
+        Array.Fill(_data, Color.Black);
+
+        RenderSpectrumChannel(0);
+
+        _spectrum.SetData(_data);
+
+        _rendering = false;
+    }
+
+    private void RenderSpectrumChannel(int channel)
+    {
+        Fourier.Forward(_buffers[channel], FourierOptions.Matlab);
+        
+        var height = Constants.ScreenHeightPixels / 4;
+
+        var magnitudes = new float[_buffers[channel].Length];
+        
+        var axis = height * Constants.SpectrumVisualisationPanelWidth * (channel + 1) - Constants.SpectrumVisualisationPanelWidth;
+
+        for (var i = 0; i < magnitudes.Length; i++)
+        {
+            magnitudes[i] = (float) Math.Sqrt(_buffers[channel][i].Real * _buffers[channel][i].Real + _buffers[channel][i].Imaginary * _buffers[channel][i].Imaginary);
+        }
+
+        for (var x = 0; x < Constants.SpectrumVisualisationPanelWidth; x++)
+        {
+            var dataPoint = magnitudes[(int) (x * ((float) BufferSize / Constants.SpectrumVisualisationPanelWidth))] / 1000;
+
+            var offset = -(int) (dataPoint * height * (channel == 3 ? 1 : 4));
+
+            _data[axis + x + offset * Constants.SpectrumVisualisationPanelWidth] = Color.LightGreen;
+        }
     }
 }
