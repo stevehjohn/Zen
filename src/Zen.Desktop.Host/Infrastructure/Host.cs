6cba19e66c77148c73737f3dcae7bafb1847b56f
Stevo John <stevehuwjohn@gmail.com>
2024-08-10 18:44:32 +0100

    WIP

--- src/Zen.Desktop.Host/Infrastructure/Host.cs
+++ src/Zen.Desktop.Host/Infrastructure/Host.cs
@@ -234,6 +234,7 @@ public class Host : Game
                 _motherboard.Fast = false;
                 _motherboard.Slow = false;
                 _motherboard.AyAudio.BufferSize = System.Modules.Audio.Constants.DefaultBufferSize;
+                AppSettings.Instance.Speed = Speed.Normal;
 
                 _motherboard.Start();
 
