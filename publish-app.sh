796a8df298c19f1bdc64b18ca11a8dda8cb218ba
Stevo John <stevehuwjohn@gmail.com>
2024-08-13 19:06:10 +0100

    WIP

--- publish-app.sh
+++ publish-app.sh
@@ -16,3 +16,28 @@ mkdir Resources
 mv MacOS/_Content Resources
 cp ../../icon.icns Resources
 cp ../../src/Zen.Desktop.Host/Info.plist .
+cd ..
+cd ..
+zip -r Zen.App.AppleSilicon.zip Zen.app
+rm -rf Zen.app
+
+dotnet publish ./src/Zen.Desktop.Host/Zen.Desktop.Host.csproj -c Release -r osx-x64 -p:PublishReadyToRun=false -p:TieredCompilation=false -p:PublishTrimmed=false --self-contained
+
+cd ./src/Zen.Desktop.Host/bin/Release/net8.0/osx-x64/publish
+pwd
+chmod +xxx Zen.Desktop.Host
+cd -
+mkdir Zen.app
+cd Zen.app
+mkdir Contents
+cd Contents
+mkdir MacOS
+cp -R ../../src/Zen.Desktop.Host/bin/Release/net8.0/osx-x64/publish/* MacOS
+mkdir Resources
+mv MacOS/_Content Resources
+cp ../../icon.icns Resources
+cp ../../src/Zen.Desktop.Host/Info.plist .
+cd ..
+cd ..
+zip -r Zen.App.Intel.zip Zen.app
+rm -rf Zen.app
