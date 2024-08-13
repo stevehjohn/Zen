8196ea22210b42b5b03eb22f5133de9751396ef5
Stevo John <stevehuwjohn@gmail.com>
2024-08-13 15:27:30 +0100

    App bundle WIP

---
+++ publish-app.sh
@@ -0,0 +1,16 @@
+dotnet publish ./src/Zen.Desktop.Host/Zen.Desktop.Host.csproj -c Release -r osx-arm64 /p:PublishReadyToRun=true /p:TieredCompilation=false --self-contained /p:PublishReadyToRunShowWarnings=true
+
+cd ./src/Zen.Desktop.Host/bin/Release/net8.0/osx-arm64/publish
+pwd
+chmod +xx Zen.Desktop.Host
+cd -
+mkdir Zen.app
+cd Zen.app
+mkdir Contents
+cd Contents
+mkdir MacOS
+cp -R ../../src/Zen.Desktop.Host/bin/Release/net8.0/osx-arm64/publish/* MacOS
+mkdir Resources
+mv MacOS/_Content Resources
+cp ../../icon.icns Resources
+cp ../../src/Zen.Desktop.Host/Info.plist .
