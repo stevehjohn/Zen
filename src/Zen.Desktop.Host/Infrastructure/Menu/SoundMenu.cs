262d02987c705cbf3483e7bb4858388efdf93915
Stevo John <stevehuwjohn@gmail.com>
2024-08-14 23:55:28 +0100

    WIP

--- src/Zen.Desktop.Host/Infrastructure/Menu/SoundMenu.cs
+++ src/Zen.Desktop.Host/Infrastructure/Menu/SoundMenu.cs
@@ -12,8 +12,9 @@ public class SoundMenu : MenuBase
         var items = new List<Label>
         {
             new(0, true, "Zen - Sound", Color.White, 0, 0, null),
-            new(1, false, $"[1] {(AppSettings.Instance.Sound ? ">" : " ")} On {(AppSettings.Instance.Sound ? "<" : " ")}", Color.Yellow, 1, 3, Keys.D1, Color.LightGreen),
-            new(2, false, $"[2] {(AppSettings.Instance.Sound ? " " : ">")} Off {(AppSettings.Instance.Sound ? " " : "<")}", Color.Yellow, 1, 5, Keys.D2, Color.LightGreen),
+            new(1, false, $"[1] {(AppSettings.Instance.Sound ? " " : ">")} Off {(AppSettings.Instance.Sound ? " " : "<")}", Color.Yellow, 1, 3, Keys.D1, Color.LightGreen),
+            new(2, false, $"[2] {(AppSettings.Instance.Sound && AppSettings.Instance.AudioEngine == AudioEngine.PortAudio ? ">" : " ")} On - PortAudio {(AppSettings.Instance.AudioEngine == AudioEngine.PortAudio ? "<" : " ")}", Color.Yellow, 1, 5, Keys.D2, Color.LightGreen),
+            new(3, false, $"[3] {(AppSettings.Instance.Sound && AppSettings.Instance.AudioEngine == AudioEngine.Bass ? ">" : " ")} On - Bass {(AppSettings.Instance.AudioEngine == AudioEngine.PortAudio ? "<" : " ")}", Color.Yellow, 1, 7, Keys.D3, Color.LightGreen),
             new(99, true, "[ESC] Close Menu", Color.FromNonPremultiplied(255, 64, 64, 255), 0, 21, Keys.Escape, Color.LightGreen)
         };
 
@@ -25,10 +26,10 @@ public class SoundMenu : MenuBase
         switch (id)
         {
             case 1:
-                return (MenuResult.SoundOn, null, null);
+                return (MenuResult.SoundOff, null, null);
 
             case 2:
-                return (MenuResult.SoundOff, null, null);
+                return (MenuResult.SoundOn, null, null);
 
             default:
                 return (MenuResult.NewMenu, new MainMenu(), null);
