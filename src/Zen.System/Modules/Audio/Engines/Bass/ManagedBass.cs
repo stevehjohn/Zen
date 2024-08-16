a85297c58cda5cb83fc740fba936340a98db4979
Stevo John <stevehuwjohn@gmail.com>
2024-08-16 16:34:51 +0100

    WIP

--- src/Zen.System/Modules/Audio/Engines/Bass/ManagedBass.cs
+++ src/Zen.System/Modules/Audio/Engines/Bass/ManagedBass.cs
@@ -4,28 +4,28 @@ namespace Zen.System.Modules.Audio.Engines.Bass;
 
 public static class ManagedBass
 {
-    [DllImport("bass")]
+    [DllImport("Libraries/libbass")]
     [return: MarshalAs((UnmanagedType) 2)]
     public static extern bool BASS_Init(int device, int freq, BassInit flags, IntPtr win);
 
-    [DllImport("bass")]
+    [DllImport("Libraries/libbass")]
     public static extern int BASS_SampleCreate(int length, int freq, int chans, int max, BassFlag flags);
 
-    [DllImport("bass")]
+    [DllImport("Libraries/libbass")]
     [return: MarshalAs((UnmanagedType) 2)]
     public static extern bool BASS_SampleSetData(int handle, float[] buffer);
     
-    [DllImport("bass", CharSet = CharSet.Unicode)]
+    [DllImport("Libraries/libbass", CharSet = CharSet.Unicode)]
     public static extern int BASS_SampleGetChannel(int handle, BassFlag flags);
     
-    [DllImport("bass")]
+    [DllImport("Libraries/libbass")]
     public static extern int BASS_ChannelSetSync(int handle, BassSync type, long param, SyncProc proc, IntPtr user);
     
-    [DllImport("bass")]
+    [DllImport("Libraries/libbass")]
     [return: MarshalAs((UnmanagedType) 2)]
     public static extern bool BASS_ChannelPlay(int handle, [MarshalAs((UnmanagedType) 2)] bool restart);
     
-    [DllImport("bass")]
+    [DllImport("Libraries/libbass")]
     [return: MarshalAs((UnmanagedType) 2)]
     public static extern bool BASS_Free();
 
