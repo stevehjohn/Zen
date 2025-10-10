75b31f268a4ae91a482f7701de478620bc6bab47
Stevo John <stevehuwjohn@gmail.com>
2025-10-10 17:41:59 +0100

    wip

--- src/Zen.Desktop.Host/Infrastructure/Menu/SystemMenu.cs
+++ src/Zen.Desktop.Host/Infrastructure/Menu/SystemMenu.cs
@@ -16,7 +16,7 @@ public class SystemMenu : MenuBase
                         new(1, false, $"[1] {(AppSettings.Instance.SystemModel == Model.Spectrum48K ? ">" : " ")} Spectrum 48K {(AppSettings.Instance.SystemModel == Model.Spectrum48K ? "<" : " ")}", Color.Yellow, 1, 3, Keys.D1, Color.LightGreen),
                         new(2, false, $"[2] {(AppSettings.Instance.SystemModel == Model.Spectrum128 ? ">" : " ")} Spectrum 128 {(AppSettings.Instance.SystemModel == Model.Spectrum128 ? "<" : " ")}", Color.Yellow, 1, 5, Keys.D2, Color.LightGreen),
                         new(3, false, $"[3] {(AppSettings.Instance.SystemModel == Model.SpectrumPlus2 ? ">" : " ")} Spectrum +2 {(AppSettings.Instance.SystemModel == Model.SpectrumPlus2 ? "<" : " ")}", Color.Yellow, 1, 7, Keys.D3, Color.LightGreen),
-                        new(4, false, $"[4] {(AppSettings.Instance.SystemModel == Model.SpectrumPlus2A ? ">" : " ")} Spectrum +2 {(AppSettings.Instance.SystemModel == Model.SpectrumPlus2A ? "<" : " ")}", Color.Yellow, 1, 9, Keys.D4, Color.LightGreen),
+                        new(4, false, $"[4] {(AppSettings.Instance.SystemModel == Model.SpectrumPlus2A ? ">" : " ")} Spectrum +2A {(AppSettings.Instance.SystemModel == Model.SpectrumPlus2A ? "<" : " ")}", Color.Yellow, 1, 9, Keys.D4, Color.LightGreen),
                         new(5, false, $"[5] {(AppSettings.Instance.SystemModel == Model.SpectrumPlus3 ? ">" : " ")} Spectrum +3 {(AppSettings.Instance.SystemModel == Model.SpectrumPlus3 ? "<" : " ")}", Color.Yellow, 1, 11, Keys.D5, Color.LightGreen),
                         new(99, true, "[ESC] Close Menu", Color.FromNonPremultiplied(255, 64, 64, 255), 0, 21, Keys.Escape, Color.LightGreen)
                     };
@@ -38,6 +38,9 @@ public class SystemMenu : MenuBase
                 return (MenuResult.Restart, null, Model.SpectrumPlus2);
 
             case 4:
+                return (MenuResult.Restart, null, Model.SpectrumPlus2A);
+
+            case 5:
                 return (MenuResult.Restart, null, Model.SpectrumPlus3);
 
             default:
