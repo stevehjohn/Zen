fd0e7b89317b076696b9616c2201d2a29131cb27
Stevo John <stevehuwjohn@gmail.com>
2025-10-12 15:33:19 +0100

    RAM

---
+++ src/Zen.Desktop.Host/Features/RamBankVisualiser.cs
@@ -0,0 +1,63 @@
+using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Zen.System.Modules;
+
+namespace Zen.Desktop.Host.Features;
+
+public class RamBankVisualiser
+{
+    private const int SideSize = 128;
+    
+    // ReSharper disable once PrivateFieldCanBeConvertedToLocalVariable
+    private readonly GraphicsDeviceManager _graphicsDeviceManager;
+
+    private readonly Color[] _data;
+
+    private readonly Texture2D _visualisation;
+
+    private Ram _ram;
+
+    public Ram Ram
+    {
+        set => _ram = value;
+    }
+
+    public RamBankVisualiser(GraphicsDeviceManager graphicsDeviceManager, Ram ram)
+    {
+        _graphicsDeviceManager = graphicsDeviceManager;
+
+        _ram = ram;
+
+        _data = new Color[SideSize * SideSize];
+
+        _visualisation = new Texture2D(_graphicsDeviceManager.GraphicsDevice, SideSize, SideSize);
+    }
+
+    public Texture2D RenderRam()
+    {
+        Array.Fill(_data, Color.Black);
+        
+        RenderBank();
+        
+        _visualisation.SetData(_data);
+
+        return _visualisation;
+    }
+
+    private void RenderBank()
+    {
+        for (var y = 0; y < SideSize; y++)
+        {
+            for (var x = 0; x < SideSize / 8; x++)
+            {
+                var data = _ram.GetBank(3)[y * SideSize + x];
+
+                for (var i = 0; i < 7; i++)
+                {
+                    _data[y * SideSize + x * 8 + i] = (data & 1 << i) > 0 ? Color.LightGray : Color.Black;
+                }
+            }
+        }
+    }
+}
