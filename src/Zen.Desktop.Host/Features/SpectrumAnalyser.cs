d35a92d0e4c7a69e8a2fd2ea9213f934698705a8
Stevo John <stevehuwjohn@gmail.com>
2025-10-17 19:21:35 +0100

    WIP

--- src/Zen.Desktop.Host/Features/SpectrumAnalyser.cs
+++ src/Zen.Desktop.Host/Features/SpectrumAnalyser.cs
@@ -161,7 +161,7 @@ public class SpectrumAnalyser
     {
         Fourier.Forward(_buffers[channel], FourierOptions.Matlab);
         
-        var height = Constants.ScreenHeightPixels / 4;
+        const int height = Constants.ScreenHeightPixels / 4;
 
         var axis = height * Constants.SpectrumVisualisationPanelWidth * (channel + 1) - Constants.SpectrumVisualisationPanelWidth;
 
