18cbee11a685311a1422340c59468abce2e210ce
Stevo John <stevehuwjohn@gmail.com>
2026-04-28 14:09:56 +0100

    Fixed warnings

--- src/Zen.Z80.Tests.Fuse/Models/TestExpectedResult.cs
+++ src/Zen.Z80.Tests.Fuse/Models/TestExpectedResult.cs
@@ -2,24 +2,16 @@
 
 public class TestExpectedResult
 {
-    public TestResultStep[] Steps { get; }
-
     public ProcessorState ProcessorState { get; }
 
     public TestExpectedResult(string[] testData)
     {
-        var steps = new List<TestResultStep>();
-
         var line = 1;
 
         while (char.IsWhiteSpace(testData[line][0]))
         {
-            steps.Add(new TestResultStep(testData[line]));
-
             line++;
         }
-
-        Steps = steps.ToArray();
 
         ProcessorState = new ProcessorState(testData[line..]);
     }
